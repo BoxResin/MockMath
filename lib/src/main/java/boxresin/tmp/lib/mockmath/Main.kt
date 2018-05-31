bfbe0d00fbe4b3157cd12ee2fb2eceb4008d7c29
Alan <BoxResin@gmail.com>
2018-05-31 12:15:27 +0900

    기본 기능 구현

---
+++ lib/src/main/java/boxresin/tmp/lib/mockmath/Main.kt
@@ -0,0 +1,9 @@
+/** 주어진 매개변수 중, 최댓값을 반환한다. */
+fun maxN(vararg nums: Int): Int {
+	return nums.max() ?: 0
+}
+
+/** 주어진 매개변수 중, 최솟값을 반환한다. */
+fun minN(vararg nums: Int): Int {
+	return nums.min() ?: 0
+}
